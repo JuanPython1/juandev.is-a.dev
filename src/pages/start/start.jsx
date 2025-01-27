00e42ab7247caf52905739e7549efa7144c038f8
JuanR <112212338+JuanPython1@users.noreply.github.com>
2025-01-27 09:43:05 -0500

    fix: :bricks: space between elongated sections the page start

--- src/pages/start/start.jsx
+++ src/pages/start/start.jsx
@@ -4,7 +4,7 @@ import Trajectory from "@/pages/start/components/Trajectory.jsx";
 export default function Start() {
 
     return (
-        <main className="mt-16 flex-grow space-y-20">
+        <main className="mt-16 flex-grow space-y-24">
 
             <section className={`flex flex-col gap-32 justify-center items-center min-w-3 max-w-2xl p-10 mx-auto rounded-e-3xl animationBlurIn`}>
                 <p className="font_juan_intro">
