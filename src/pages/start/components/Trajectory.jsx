0edc39c52b6054e2ba3ed48b505bec091e91625b
JuanR <112212338+JuanPython1@users.noreply.github.com>
2025-02-03 16:58:46 -0500

    fix: :art: new trajectory animation-timeline scroll

--- src/pages/start/components/Trajectory.jsx
+++ src/pages/start/components/Trajectory.jsx
@@ -1,42 +1,39 @@
 import IconsContainer from "@/components/IconsContainer";
 import trayectoriaData from "@data/Trajectory.json";
+import '@pages/start/styles/Trayectory.css';
 
 export default function Trajectory() {
     const events = trayectoriaData.events;
 
     return (
-        <div className="flex flex-col justify-center items-center w-screen md:flex-row overflow-x-auto space-y-5 md:space-x-14 md:space-y-0">
-            {events.map((event, index) => {
-                return (
-                    <div key={index} className="flex flex-col justify-center items-center">
+        <div className="relative flex flex-col items-center w-full ">
+            {/* Línea de tiempo central */}
+            <div className="absolute left-1/2 w-0.5 h-full bg-zinc-500 dark:bg-[#d95a5a] transform -translate-x-1/2"></div>
 
-                        <div
-                            className="flex flex-col gap-2  h-32 bg-zinc-800 dark:bg-[#d95a5a]  transition-colors duration-500 justify-center items-center my-3 md:my-5 p-3 w-[250px] min-h-[250px] min-w-[250px]"
-                        >
-                            <div className="flex h-1/2 items-center">
-                                <p className="font_juan_text_trajectory">{event.description}</p>
-                            </div>
-                            <div className="flex flex-col h-1/2 items-center gap-2">
-                                <p className="font_juan_framework_trajectory">{event.toolTitle}</p>
-                                <div className="flex flex-row gap-1 w-full justify-center items-center">
-                                    {Object.keys(event.framework).map((key) => (
-                                        <IconsContainer
-                                            key={key}
-                                            url={event.framework[key].icon}
-                                            alt={event.framework[key].alt}
-                                        />
-                                    ))}
-                                </div>
+            {/* Punto en la línea de tiempo */}
+            <div className="triangle animation"></div>
 
-                                <h1 className="text-2xl font_juan_tittle_trajectory">{event.year}</h1>
-                            </div>
+            {events.map((event, index) => (
+                <div key={index} className={`relative flex w-full md:w-3/4 py-5 ${index % 2 === 0 ? 'justify-start' : 'justify-end'}`}>
+
+
+                    {/* Tarjeta de evento */}
+                    <div className="flex flex-col gap-2 bg-zinc-800 dark:bg-[#d95a5a] transition-colors duration-500 p-5 w-[250px] min-h-[250px] shadow-lg z-10">
+                        <p className="font_juan_text_trajectory">{event.description}</p>
+                        <p className="font_juan_framework_trajectory text-center">{event.toolTitle}</p>
+                        <div className="flex flex-row gap-1 w-full justify-center items-center">
+                            {Object.keys(event.framework).map((key) => (
+                                <IconsContainer
+                                    key={key}
+                                    url={event.framework[key].icon}
+                                    alt={event.framework[key].alt}
+                                />
+                            ))}
                         </div>
-
+                        <h1 className="text-2xl font_juan_tittle_trajectory text-center">{event.year}</h1>
                     </div>
-                );
-            })}
-
-
+                </div>
+            ))}
         </div>
     );
 }
--- src/pages/start/start.jsx
+++ src/pages/start/start.jsx
@@ -4,7 +4,7 @@ import Trajectory from "@/pages/start/components/Trajectory.jsx";
 export default function Start() {
 
     return (
-        <main className="mt-16 flex-grow space-y-32">
+        <main className="mt-16 flex-grow space-y-36">
 
             <section className={`flex flex-col gap-32 justify-center items-center min-w-3 max-w-2xl p-10 mx-auto rounded-e-3xl animationBlurIn`}>
                 <p className="font_juan_intro">
@@ -15,8 +15,8 @@ export default function Start() {
                 <FrameworksCube />
             </section>
 
-            <section className="flex flex-col md:flex-row flex-grow gap-16 md:gap-24 justify-center items-center max-w-[1200px] p-10 mx-auto animationBlurIn">
-                <h1 className="text-center md:text-2xl font_juan_tittle">Learning Trajectory</h1>
+            <section className="flex flex-col gap-16 md:gap-24 justify-center items-center max-w-[1000px] p-10 mx-auto animationBlurIn">
+                <h1 className="text-center md:text-3xl font_juan_tittle">Learning Trajectory</h1>
                 <Trajectory />
             </section>
 
