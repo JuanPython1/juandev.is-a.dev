4f0768e332e2a58220453cce1678c16b02d40aaa
JuanR <112212338+JuanPython1@users.noreply.github.com>
2025-02-04 11:37:24 -0500

    Updates

--- assets/Layout-CYPHEVkq.js
+++ assets/Layout-CYPHEVkq.js
@@ -1,4 +1,4 @@
-import{j as e,r as o,u as k,L as b,O as v}from"./index-Cbm3s_uw.js";import{G as m}from"./iconBase-CZJUxfwx.js";function j(){return e.jsx("div",{className:"w-full flex justify-center h-56 mt-20 font_juan_footer",children:e.jsx("p",{children:"©2025 SrJuan.dev - Developer Web & Mobile. All rights reserved."})})}/**
+import{j as e,r as o,u as k,L as b,O as v}from"./index-DICL4x8E.js";import{G as m}from"./iconBase-BWFiDxK-.js";function j(){return e.jsx("div",{className:"w-full flex justify-center h-56 mt-20 font_juan_footer",children:e.jsx("p",{children:"©2025 SrJuan.dev - Developer Web & Mobile. All rights reserved."})})}/**
  * @license lucide-react v0.445.0 - ISC
  *
  * This source code is licensed under the ISC license.
