88c68d1313e99863fc19d21955c33d94c3d219b6
JuanR <112212338+JuanPython1@users.noreply.github.com>
2025-02-07 23:49:07 -0500

    fix: :art: new animation intro start hi, it's Juan's website

--- src/pages/start/start.jsx
+++ src/pages/start/start.jsx
@@ -1,18 +1,28 @@
 import FrameworksCube from "@/pages/start/components/FrameworksCube.jsx";
 import Trajectory from "@/pages/start/components/Trajectory.jsx";
+import './styles/start.css';
 
 export default function Start() {
 
     return (
-        <main className="mt-16 flex-grow space-y-36">
+        <main className="flex-grow space-y-40">
 
-            <section className={`flex flex-col gap-32 justify-center items-center min-w-3 max-w-2xl p-10 mx-auto rounded-e-3xl animationBlurIn`}>
-                <p className="font_juan_intro">
-                    {
-                        `"I am a Systems Engineering student, currently in my tenth semester. I specialize in web and mobile development with React with the Next.js framework and React Native with the Expo framework. Recently, I have been exploring the integration of artificial intelligence in my projects, leveraging AI technologies to improve user experiences and streamline development processes."`
-                    }
-                </p>
-                <FrameworksCube />
+            <section className="relative flex flex-col gap-16 min-w-3 max-w-2xl h-full p-10 mx-auto overflow-hidden">
+
+                <div className="absolute flex justify-center items-center w-full h-full top-0 left-0">
+                    <h1 className="font_juan_tittle_SrJuan title-animation">
+                        {`Hi, it's Juan's website 🐋`}
+                    </h1>
+                </div>
+
+                <div className=" flex flex-col gap-28 mt-16 md:h-[500px] items-center content-animation">
+                    <p className="font_juan_intro">
+                        {
+                            `"I am a Systems Engineering student, currently in my tenth semester. I specialize in web and mobile development with React with the Next.js framework and React Native with the Expo framework. Recently, I have been exploring the integration of artificial intelligence in my projects, leveraging AI technologies to improve user experiences and streamline development processes."`
+                        }
+                    </p>
+                    <FrameworksCube />
+                </div>
             </section>
 
             <section className="flex flex-col gap-16 md:gap-24 justify-center items-center max-w-[1000px] p-10 mx-auto animationBlurIn">
