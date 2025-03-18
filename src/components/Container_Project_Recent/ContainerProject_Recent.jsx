ee345c152d5f2e9e3e25d0bc642e4a905c87dc2b
JuanR <112212338+JuanPython1@users.noreply.github.com>
2025-03-18 13:46:16 -0500

    fix: :art: color arrangement and base structure of recent projects

--- src/components/Container_Project_Recent/ContainerProject_Recent.jsx
+++ src/components/Container_Project_Recent/ContainerProject_Recent.jsx
@@ -1,34 +1,37 @@
 import { IoGitBranchOutline } from "react-icons/io5";
 import { MdOutlineArrowOutward } from "react-icons/md";
-
 import ButtonProject from "@components/buttonProject/ButtonProject";
 
 const ContainerProject_Recent = ({ Tittle, Description, Image, Github, Deploy }) => {
     return (
-        <div className="flex flex-row justify-center gap-6 items-center text-center w-[672px] h-[480px] border border-red-300 rounded-lg">
-        <div className='flex justify-center items-center w-[400px] h-[500px] bg-slate-500 rounded-md overflow-hidden'>
-            <img src={Image} alt={Tittle} className="w-full h-full object-cover" />
-        </div>
+        <div className="flex sm:flex-row flex-col justify-center gap-6 items-center sm:w-[672px] sm:h-[480px] w-full h-auto border border-red-300 dark:border-red-200 rounded-lg p-4">
+            {/* Imagen del Proyecto */}
+            <div className="flex justify-center items-center sm:w-[570px] sm:h-[500px] w-full h-[300px] bg-slate-500 border border-red-300 dark:border-red-200 rounded-md overflow-hidden">
+                <img src={Image} alt={Tittle} className="w-full h-full object-cover" />
+            </div>
 
-            <div className="flex flex-col justify-center gap-4 items-center w-[180px] h-[190px]">
-                <div className="flex flex-col justify-center items-center h-[50px] ">
+            {/* Contenido del Proyecto */}
+            <div className="flex flex-col justify-center gap-4 sm:gap-12 items-center sm:items-start sm:w-[200px] sm:h-auto w-full h-auto">
+                {/* Título */}
+                <div className="flex flex-col justify-center items-center sm:items-start h-[50px]">
                     <h2 className="font_juan_title_projects">{Tittle}</h2>
                 </div>
-                <div className="flex flex-col justify-center items-center h-auto">
-                    <p className="text-red-400 dark:text-red-300">{Description}</p>
+                {/* Descripción */}
+                <div className="flex flex-col justify-center items-center text-justify sm:text-start sm:items-center h-auto w-[80%] sm:w-full sm:h-[100px] mt-2 mb-4">
+                    <p className="text-red-400 dark:text-red-200 text-sm">{Description}</p>
                 </div>
-                <div className="flex flex-row gap-2 justify-center items-center w-[180px] h-[70px] ">
+                {/* Botones */}
+                <div className="flex flex-row gap-2 justify-center sm:justify-start items-last w-[180px] h-[70px] sm:w-full">
                     <ButtonProject url={Github}>
-                        <IoGitBranchOutline width={10} height={10} />
+                        <IoGitBranchOutline size={18} />
                     </ButtonProject>
-
                     <ButtonProject url={Deploy}>
-                        <MdOutlineArrowOutward width={10} height={10} rotate={90} />
+                        <MdOutlineArrowOutward size={18} />
                     </ButtonProject>
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default ContainerProject_Recent
+export default ContainerProject_Recent;
--- src/components/buttonProject/ButtonProject.jsx
+++ src/components/buttonProject/ButtonProject.jsx
@@ -3,7 +3,7 @@
 const ButtonProject = ({ children, url }) => {
     return (
         <a href={url} target="_blank" 
-        rel="noopener noreferrer"  className="flex justify-center items-center w-[40px] h-[40px] rounded-lg bg-[#c04b4b] hover:bg-red-400 dark:bg-red-300 dark:hover:bg-red-400" >
+        rel="noopener noreferrer"  className="flex justify-center items-center w-[30px] h-[30px] rounded-lg bg-[#c04b4b] hover:bg-red-400 dark:bg-red-300 dark:hover:bg-red-400" >
             {children}
         </a>
     )
