a7cb4889f2a1a7060af928533e6a77ba4f176321
JuanR <112212338+JuanPython1@users.noreply.github.com>
2026-07-20 15:51:02 -0500

    style: :art: correct invalid color-stop order in dot-grid background
    correct invalid color-stop order in dot-grid background

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -7,7 +7,7 @@ import Loading from './Loading'
 
 export default function Layout() {
     return (
-        <div className="flex flex-col min-h-screen bg-[#242424] dark:bg-[#c04b4b] transition-colors duration-500 bg-[radial-gradient(circle,_#000_0.5px,_transparent_0.1px)] bg-[size:10px_10px]">
+        <div className="flex flex-col min-h-screen bg-[#242424] dark:bg-[#c04b4b] transition-colors duration-500 bg-[radial-gradient(circle,_#000_1px,_transparent_1px)] bg-[size:20px_20px]">
 
             <header>
                 <Nav />
