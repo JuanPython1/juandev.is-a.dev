94647212ec259139a8f0ea38f7752eb205a9b4dd
JuanR <112212338+JuanPython1@users.noreply.github.com>
2026-08-03 18:38:59 -0500

    Updates

---
+++ assets/about-B6PnZpyA.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-C_A88nrP.js";import{u as e}from"./useTranslation-qW6zuish.js";function s(){const{t:o}=e();return t.jsx("section",{className:"flex w-max h-max my-[14em] mx-auto animationBlurIn zoom-scale-root",children:t.jsx("h1",{className:"text-[1.5em] md:text-[2.25em] text-red-300 font-iosevka",children:o("about.comingSoon")})})}export{s as default};
