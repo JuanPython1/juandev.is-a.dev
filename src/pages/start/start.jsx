68f79c4b336aaaac2ddadef0d6a09d817df322af
JuanR <112212338+JuanPython1@users.noreply.github.com>
2026-08-03 13:22:12 -0500

    style: :art: Tighten spacing in Start between cube, recent projects, and their titles
    Tighten spacing in Start between cube, recent projects, and their titles

--- src/pages/start/start.jsx
+++ src/pages/start/start.jsx
@@ -8,7 +8,7 @@ export default function Start() {
     const { t } = useTranslation();
 
     return (
-        <main className="flex-grow space-y-16 md:space-y-40">
+        <main className="flex-grow">
 
             <section className="relative flex flex-col gap-[3em] min-w-3 max-w-[32em] sm:min-h-screen p-[2em] mx-auto overflow-hidden zoom-scale-root">
 
@@ -26,11 +26,11 @@ export default function Start() {
                 </div>
             </section>
 
-            <section className="section-blur animationBlurIn max-w-[31.25em] zoom-scale-root">
+            <section className="section-blur animationBlurIn max-w-[31.25em] zoom-scale-root mt-10 md:mt-24">
                 <RecentProyectsContainer />
             </section>
 
-            <section className="section-blur animationBlurIn max-w-[62.5em] zoom-scale-root">
+            <section className="section-blur animationBlurIn max-w-[62.5em] zoom-scale-root mt-16 md:mt-40">
                 <h1 className="text-center font_juan_tittle">{t('start.trajectoryTitle')}</h1>
                 <Trajectory />
             </section>
